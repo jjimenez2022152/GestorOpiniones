dc5dc7bfe882f4bcd3e82103adbf7d5f6abba6ed
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-03-02 02:34:52 -0600

    Se crea el modelo de comentario

---
+++ src/coments/coment.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const ComentarioSchema = mongoose.Schema({
+    texto: {
+        type: String,
+        required: true
+    },
+    usuario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: true
+    },
+    publicacion: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Publicacion',
+        required: true
+    }
+});
+
+export default mongoose.model('Comentario', ComentarioSchema);
