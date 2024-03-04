0e49b530f65e2278d259d184c486ef6cf8ed5c92
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-03-03 18:42:08 -0600

    Se agrega contenido a coment.routes

--- src/coments/coment.routes.js
+++ src/coments/coment.routes.js
@@ -1,20 +1,39 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { agregarComentario } from "./coment.controller.js"; // Importa el controlador de comentarios
+import { agregarComentario, getComments,
+     commentPut, commentDelete } from "./coment.controller.js";
 import { validarCampos } from "../middlewares/validarCampos.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Router();
 
+router.get("/", getComments);
+
 router.post(
     "/crear",
     [
         check("comentario", "El comentario es requerido").not().isEmpty(),
-        check("idPublicacion", "El ID de la publicación es requerido").not().isEmpty(), // Corregido el nombre del parámetro
+        check("idPublicacion", "El ID de la publicación es requerido").not().isEmpty(), 
         validarCampos,
         validarJWT
     ],
     agregarComentario
+);
+
+router.put(
+    "/:id",
+    [
+        validarJWT,
+        check("comentario", "El comentario es obligatorio").not().isEmpty(),
+        validarCampos
+    ],
+    commentPut
+);
+
+router.delete(
+    "/:id",
+    validarJWT, 
+    commentDelete
 );
 
 export default router;
